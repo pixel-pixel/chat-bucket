a6e811c8d9f64c93e9b4d1854006973957f1fb94
pixel-pixel <bartishco@gmail.com>
2021-10-05 13:09:40 +0300

    [init]

---
+++ client/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ hello }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import { Context } from '@nuxt/types';
+
+@Component({
+  name: 'Index'
+})
+export default class Index extends Vue {
+  private hello!: string;
+
+  async asyncData({ $axios }: Context) {
+    let hello;
+    try {
+      hello = await $axios.$get('/');
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+    }
+
+    return {
+      hello
+    };
+  }
+}
+</script>
