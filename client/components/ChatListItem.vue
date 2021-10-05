dabba58e37ec8fe633c6b9143419475d50a36d3c
pixel-pixel <bartishco@gmail.com>
2021-10-05 19:00:37 +0300

    [created] User list item comonent

---
+++ client/components/ChatListItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="chat-li">
+    <img class="photo" :src="imgURL" />
+    <div class="online" v-show="!online" />
+
+    <div class="text-div">
+      <h6 class="name">{{ name }}</h6>
+      <p class="last-msg">{{ lastMsg }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { Context } from '@nuxt/types';
+
+@Component({name: 'ChatListItem'})
+export default class Index extends Vue {
+  @Prop() name = 'No Name'
+  @Prop() imgURL = 'https://stekoltd.kz/assets/img/otzivi/hab.jpg'
+  @Prop() online = false
+  @Prop() lastMsg = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+}
+</script>
+
+<style scoped>
+  .chat-li {
+    display: flex;
+    width: 100%;
+    position: relative;
+    padding: .5rem 1rem;
+
+    background-color: white;
+  }
+  .chat-li:hover {
+     background-color: lightgray;
+  }
+
+  .photo {
+    width: 5rem;
+    height: 5rem;
+
+    border-radius: .25rem;
+  }
+
+  .online {
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+    top: 4.7rem;
+    left: 5.2rem;
+
+    border-radius: .5rem;
+    background-color: rgb(0, 255, 0);
+  }
+
+  .text-div {
+    padding-top: .5rem;
+    margin: 0 1.5rem;
+  }
+  .name,
+  .last-msg {
+    line-height: 100%;
+  }
+  .name {
+    font-size: 1.5rem;
+  }
+  .last-msg {
+    height: 2rem;
+    color: grey;
+    font-size: 1.25rem;
+  }
+
+  
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,29 +1,25 @@
 <template>
-  <div>{{ hello }}</div>
+  <div>
+    <ChatListItem v-for="name in arr" :key="name">
+      {{name}}
+    </ChatListItem>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
 import { Context } from '@nuxt/types';
 
-@Component({
-  name: 'Index'
-})
+@Component({name: 'Index'})
 export default class Index extends Vue {
-  private hello!: string;
-
-  async asyncData({ $axios }: Context) {
-    let hello;
-    try {
-      hello = await $axios.$get('/');
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-    }
-
-    return {
-      hello
-    };
-  }
+  arr = ['kek', 'lol', 'lalka']
 }
 </script>
+
+<style>
+  * { 
+    font-family: 'Helvetica', 'Arial', sans-serif; 
+    padding: 0;
+    margin: 0;
+  }
+</style>
