9f9f7e2ce9f7c0280a94649bdbec92dc3363e8bb
pixel-pixel <bartishco@gmail.com>
2021-10-06 18:59:44 +0300

    [readded] Sockets

--- client/plugins/socket.io.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import { io } from 'socket.io-client';
-import VueSocketIOExt from 'vue-socket.io-extended';
-
-const socket = io('http://localhost:8080');
-
-Vue.use(VueSocketIOExt, socket);
